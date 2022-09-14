a86ff04bf438209081c5b81db10191d2d3c2ce42
Curtis Becker <ceb2941@gmail.com>
2022-09-14 15:03:36 -0400

    Initial Commit

---
+++ CrudPractice/CrudPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CrudPracticeApp.swift
+//  CrudPractice
+//
+//  Created by admin on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct CrudPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
