20249e7dfd10e38c0a9d4cf6636df03d70b81e06
Curtis Becker <ceb2941@gmail.com>
2022-09-19 23:14:29 -0400

    removed comment

--- CrudPractice/View/Embedded/BackgroundView.swift
+++ CrudPractice/View/Embedded/BackgroundView.swift
@@ -9,7 +9,6 @@ import SwiftUI
 
 struct BackgroundView: View {
     var body: some View {
-        //would be easier to just use an image as background
         ZStack{
             //Safe Area
             Color(.red)
@@ -25,6 +24,6 @@ struct BackgroundView: View {
 struct BackgroundView_Previews: PreviewProvider {
     static var previews: some View {
         BackgroundView()
-.previewInterfaceOrientation(.landscapeRight)
+.previewInterfaceOrientation(.portrait)
     }
 }
